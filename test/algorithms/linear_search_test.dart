8cdbf18c51cd02615854592ae710fa8eaac249a3
岳琪 <yueqiqi@xiaomi.com>
2021-09-04 10:20:50 +0800

    [Algorithms] add commit
    Signed-off-by: 岳琪 <yueqiqi@xiaomi.com>

--- test/algorithms/linear_search_test.dart
+++ test/algorithms/linear_search_test.dart
@@ -2,8 +2,15 @@ import 'package:flutter_unit/algorithms/linear_search.dart';
 import 'package:flutter_unit/utils/logs/logs.dart';
 import 'package:flutter_unit/utils/utils.dart';
 
+///
+/// 测试线程查找的性能
+/// 结论：时间复杂度 O（N）
+/// 1. main n = 1000000, 100 runs: Spend time 0.061863s
+/// 2. main n = 10000000, 100 runs: Spend time 0.74305s
+/// 3. main n = 100000000, 100 runs: Spend time 7.154954s
+///
 void main() {
-  final dataSize = [1000000, 10000000];
+  final dataSize = [1000000, 10000000, 100000000];
 
   for (final n in dataSize) {
     final list = ArrayGenerator.generateOrderedArray(n);
