24143d12a7be948b0d9fd44f3bdde07d8ff72707
岳琪 <yueqiqi@xiaomi.com>
2021-09-04 21:12:05 +0800

    [Algorithms] new select sort part.3
    Signed-off-by: 岳琪 <yueqiqi@xiaomi.com>

--- lib/algorithms/select_sort.dart
+++ lib/algorithms/select_sort.dart
@@ -2,13 +2,29 @@ import 'package:flutter_unit/algorithms/algorithms.dart';
 import 'package:flutter_unit/utils/utils.dart';
 import 'package:collection/collection.dart';
 
+enum SelectSortType {
+  normal,
+  normalReverse,
+  situ,
+}
+
 class SelectSort<T> extends Sort<T> {
+  final SelectSortType type;
+
   SelectSort({
+    this.type = SelectSortType.normal,
     int Function(T, T) compare = defaultCompare,
   }) : super(compare);
 
   @override
   void sort(List<T> list) {
+    if (type == SelectSortType.normal) return _normal(list);
+    if (type == SelectSortType.normalReverse) return _normalReverse(list);
+
+    return _situ(list);
+  }
+
+  void _normal(List<T> list) {
     for (int i = 0; i < list.length; i++) {
       var minIndex = i;
       for (int j = i; j < list.length; j++) {
@@ -20,23 +36,36 @@ class SelectSort<T> extends Sort<T> {
     }
   }
 
-  static List<int> situ(List<int> list) {
-    final newList = List.filled(list.length, 0);
+  void _normalReverse(List<T> list) {
+    for (int i = list.length - 1; i >= 0; i--) {
+      var maxIndex = i;
+      for (int j = i; j >= 0; j--) {
+        if (compare(list[j], list[maxIndex]) > 0) {
+          maxIndex = j;
+        }
+      }
+      list.swap(i, maxIndex);
+    }
+  }
+
+  void _situ(List<T> list) {
+    final newList = List<T>.filled(list.length, list[0]);
+    final visit = List<bool>.filled(list.length, false);
 
     for (int i = 0; i < list.length; i++) {
-      var minValue = AlgorithmsUtils.intMax;
-      var minIndex = -1;
+      var minIndex = 0;
       for (int j = 0; j < list.length; j++) {
-        if (list[j] < minValue) {
-          minValue = list[j];
+        if (!visit[j] && compare(list[j], list[minIndex]) < 0) {
           minIndex = j;
         }
       }
-      list[minIndex] = AlgorithmsUtils.intMax;
-      newList[i] = minValue;
+      newList[i] = list[minIndex];
+      visit[minIndex] = true;
     }
 
-    return newList;
+    for (int i = 0; i < list.length; i++) {
+      list[i] = newList[i];
+    }
   }
 
   @override
